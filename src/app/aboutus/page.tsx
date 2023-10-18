2a26492280bf4ea026bafd98287268a0f3875cde
kumaradityaraj <sedulous.0007@gmail.com>
2023-10-18 11:19:48 +0530

    Merging the files

--- src/app/aboutus/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-
-const Aboutus = () => {
-  return <div>Aboutus</div>;
-};
-
-export default Aboutus;
--- src/components/Catalog/CatalogContainer.test.tsx
+++ src/components/Catalog/CatalogContainer.test.tsx
@@ -2,7 +2,11 @@
  * @jest-environment jsdom
  */
 import { render, screen } from '@testing-library/react';
+<<<<<<< Updated upstream
 import Gallery from '../../app/connectors/select-connector/page.client.stories';
+=======
+import Gallery from '@/app/(connectors)/select-connector/page.client.stories';
+>>>>>>> Stashed changes
 import CatalogContainer from '@/components/Catalog/CatalogContainer';
 
 describe('CatalogContainer', () => {
